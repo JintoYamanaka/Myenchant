d8eba6cbb62ec7c43414a0179795b54e859c144a
JintoYamanaka <34837803+JintoYamanaka@users.noreply.github.com>
2018-07-14 12:23:52 +0900

    git 整理

---
+++ enchant/main.js
@@ -0,0 +1,57 @@
+enchant();
+
+/*
+
+Core ゲーム本体
+- rootScene ゲームシーン
+-- Sprite (bear) 部品
+
+*/
+
+window.onload = function() {
+  var core = new Core(320, 320);
+  core.preload('chara1.png');
+  core.fps = 15
+  core.onload = function() {
+
+    // var bear = new Sprite(32, 32);
+    // bear.image = core.assets['chara1.png'];
+    // bear.x = 0;
+    // bear.y = 0;
+    // // bear.frame = 1;
+    // bear.addEventListener('enterframe', function() {
+    //   if(core.input.right) this.x += 5;
+    // });
+    // core.rootScene.addChild(bear);
+
+    var Bear = Class.create(Sprite, {
+      initialize: function(x, y) {
+        Sprite.call(this, 32, 32);
+        this.x = x;
+        this.y = y;
+        this.frame = rand(5);
+        this.opacity = rand(100) / 100;
+        this.image = core.assets['chara1.png'];
+
+        this.tl.moveBy(rand(100), 0, 40, enchant.Easing.BOUNCE_EASEOUT)
+               .moveBy(-rand(100), -rand(20), rand(20))
+               .fadeOut(20)
+               .fadeIn(10)
+               .loop;
+
+        core.rootScene.addChild(this);
+      }
+    })
+
+    var bears = [];
+    for (var i = 0; i < 100; i++) {
+      bears[i] = new Bear(rand(320), rand(320));
+    }
+
+  }
+  core.start();
+};
+
+function rand(n) {
+  return Math.floor(Math.random() * (n+1));
+}
